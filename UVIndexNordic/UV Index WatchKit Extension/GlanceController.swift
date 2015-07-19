2c619b8d287844dc2f7e460e156b9b7f5c2749c7
Per Jansson <per.r.jansson@gmail.com>
2015-07-19 12:21:01 +0200

    Added Apple Watch target to project

---
+++ UVIndexNordic/UV Index WatchKit Extension/GlanceController.swift
@@ -0,0 +1,31 @@
+//
+//  GlanceController.swift
+//  UV Index WatchKit Extension
+//
+//  Created by Per Jansson on 2015-07-19.
+//  Copyright (c) 2015 Per Jansson. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class GlanceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
